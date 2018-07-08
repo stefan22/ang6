ac711a931a089f553cb966564c05f7249d78b1ec
slissia <slissia@gvcgroup.com>
2018-07-08 20:04:39 +0100

    updated nav

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  color: string = '#9d9d9d';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onClickChange(e) {
+    var tar = e.target.textContent.trim();
+    if (tar == 'Ng6') {
+      if (this.color == '#9d9d9d') {
+        this.color = 'red';
+      } else {
+        this.color = '#9d9d9d';
+      }
+    }
   }
-
-}
+} // NavComponent class
